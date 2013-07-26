1e0a21e1b33462a6d3fa89095e317983902d6b4f
mde17 <marcjericespiritu@gmail.com>
2013-07-26 16:58:34 +0800

    updated animation bugs
    animation bugs which results from fast changing of slides

--- animated-slider.js
+++ animated-slider.js
@@ -28,16 +28,19 @@
         this.html( function() {
             return '<div class="slider-contents">'+collectedData.cur_html+'</div>';
         });
+        
+        //display page nav BEGIN
         this.append('<div class="slide-nav"></div>');
         this.find('.slider-contents').children('div').addClass('slides');
         this.find(".slider-contents,.slider-contents .slides").css({'width':settings.width,'height':settings.height});
         for(var i = 0; i < collectedData.totalSlides.length;i++){
 			this.find('.slide-nav').append('<a>'+i+'</a>');
 		}
-		
+		//display page nav END
+
 		//display slider pages if slides are more than 1 BEGIN
 		if(collectedData.totalSlides.length > 1){
-			this.append('<div class="slider-controller"><a class="prev">prev</a><a class="next">next</a></div>');
+			this.append('<div class="slider-controller"><a class="slider-prev">prev</a><a class="slider-next">next</a></div>');
 		}
 		//display slider pages if slides are more than 1 END
 		
@@ -51,7 +54,7 @@
 		//slider page clicked END
 		
 		//next button clicked BEGIN
-		this.find('.slider-controller .next').on("click",function(){
+		this.find('.slider-controller .slider-next').on("click",function(){
 			clearInterval(slideTimer);
 			slideTimer = window.setInterval(playSlide,settings.slide_interval);
 			curSlide = curSlide;
@@ -60,7 +63,7 @@
 		//next button clicked END
 		
 		//prev button clicked BEGIN
-		this.find('.slider-controller .prev').on("click",function(){
+		this.find('.slider-controller .slider-prev').on("click",function(){
 			clearInterval(slideTimer);
 			slideTimer = window.setInterval(playSlide,settings.slide_interval);
 			
@@ -93,13 +96,24 @@
 		//Play Slides BEGIN
 		function playSlide(){
 				displayLoader();
-				for(var i = 0; i < settings.slide_animation_data.length;i++){
+				console.log(settings.slide_animation_data);
+				console.log(curSlide);
+				console.log(settings.slide_animation_data[0][curSlide]);
+				/*for(var i = 0; i < settings.slide_animation_data.length;i++){
 					slideAnimation(
 							settings.slide_animation_data[i].slide_number,
 							settings.slide_animation_data[i].target_slide_element,
 							settings.slide_animation_data[i].animation_position,
 							settings.slide_animation_data[i].animation_type
 						);
+				}*/
+				for(var i =0;i<settings.slide_animation_data[0][curSlide].length;i++){
+								slideAnimation(
+									curSlide+1,
+									settings.slide_animation_data[0][curSlide][i].target_slide_element,
+									settings.slide_animation_data[0][curSlide][i].animation_position,
+									settings.slide_animation_data[0][curSlide][i].animation_type
+								);
 				}
 				slider.find('.slider-contents').children('div').css('display','none');
 				slider.find('.slide'+(curSlide+1)).css('display','block');
@@ -112,12 +126,17 @@
 		//Play Slides END
 		
 		//slide animation function BEGIN
+		var slide_animation_timeout;
 		function slideAnimation(slide_number,target_slide_element,animation_position,animation_type){
-			slider.find('.slide'+slide_number+' '+target_slide_element).removeClass('animated '+animation_type).css('display','none');
-			setTimeout(function(){slider.find('.slide'+slide_number+' '+target_slide_element).addClass('animated '+animation_type).css('display','block');},(animation_position*1000));
+			slider.find('.slide'+slide_number+' '+target_slide_element).removeClass().css('display','none').addClass(helperReplaceChar(target_slide_element));
+			slide_animation_timeout = setTimeout(function(){slider.find('.slide'+slide_number+' '+target_slide_element).addClass('animated '+animation_type).css('display','block');},(animation_position*1000));
 		}
 		//slide animation function END
 
+		function helperReplaceChar(s_char){
+			return s_char.replace(/.|#|h1|p||div|ul|li|ol|span/,' ');
+		}
+		
 		//execute slide
 		playSlide();
     }
